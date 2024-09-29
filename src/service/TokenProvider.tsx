94449dd4b92a34b1a17c708f956b80718e857863
sokphaladam <sokphaladam@gmail.com>
2024-09-29 17:47:54 +0700

    update

--- src/service/TokenProvider.tsx
+++ src/service/TokenProvider.tsx
@@ -1,24 +1,24 @@
-"use client";
-import React, { useState } from "react";
-import { deleteCookie, getCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-import { MeQuery, useMeQuery } from "@/gql/graphql";
-import { Loading } from "@/components/custom/Loading";
-import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+'use client';
+import React, { useState } from 'react';
+import { deleteCookie, getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { MeQuery, useMeQuery } from '@/gql/graphql';
+import { Loading } from '@/components/custom/Loading';
+import { TypewriterEffectSmooth } from '@/components/ui/typewriter-effect';
 
 interface Props {
   onCompleted?: (success: boolean) => void;
 }
 
 export function TokenVerification(props: Props) {
-  const token = getCookie("tk_token");
+  const token = getCookie('tk_token');
   const { push } = useRouter();
   const [load, setLoad] = useState(true);
 
   const resetToken = async (res: MeQuery) => {
     if (token && res.me === null) {
-      await deleteCookie("tk_token");
-      await push("/");
+      await deleteCookie('tk_token');
+      await push('/');
       if (process.browser) {
         setTimeout(() => {
           window.location.reload();
@@ -42,7 +42,7 @@ export function TokenVerification(props: Props) {
           props.onCompleted && props.onCompleted(false);
           setLoad(false);
         }
-      }, 500)
+      }, 500);
     },
   });
 
@@ -52,27 +52,27 @@ export function TokenVerification(props: Props) {
 
   const words = [
     {
-      text: "Welcome ",
-      className: "text-blue-500 dark:text-blue-500"
+      text: 'Welcome ',
+      className: 'text-yellow-900 dark:text-yellow-900',
     },
     {
-      text: "to",
-      className: "text-blue-500 dark:text-blue-500"
+      text: 'to',
+      className: 'text-yellow-900 dark:text-yellow-900',
     },
     {
-      text: "application",
-      className: "text-blue-500 dark:text-blue-500"
+      text: 'The',
+      className: 'text-yellow-900 dark:text-yellow-900',
     },
     {
-      text: "Loading...",
-      className: "text-blue-500 dark:text-blue-500",
+      text: 'Mood...',
+      className: 'text-yellow-900 dark:text-yellow-900',
     },
   ];
 
   return (
     <div
       className="fixed w-screen h-screen bg-slate-100 z-[999] flex justify-center items-center bg-login flex-col"
-      style={{ display: loading || load ? "" : "none" }}
+      style={{ display: loading || load ? '' : 'none' }}
     >
       <div>
         <Loading />
