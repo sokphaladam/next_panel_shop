b61f06aac53aa7507ca3079af114f90dbbc9dcd9
sokphaladam <sokphaladam@gmail.com>
2025-10-06 01:37:18 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ git fetch origin deploy-local
 if [ $(git rev-parse HEAD) != $(git rev-parse origin/deploy-local) ]; then
   echo "🔄 Changes detected, updating..."
   git pull origin deploy-local
-  . $HOME/.nvm/nvm.sh
-  npm install --omit=dev
-  npm run build
-  pm2 restart next_panel_shop
+  . ~/.nvm/nvm.sh && npm install --omit=dev
+  . ~/.nvm/nvm.sh && npm run build
+  . ~/.nvm/nvm.sh && pm2 restart next_panel_shop
   echo "✅ Updated successfully!"
 else
   echo "✅ No changes, skipping build."
