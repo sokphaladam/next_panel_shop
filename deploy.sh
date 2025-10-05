742593787af31f273cabc39290dd553d2acb66da
sokphaladam <sokphaladam@gmail.com>
2025-10-06 00:54:36 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd /var/www/next_panel_shop
 git fetch origin deploy-local
 if [ $(git rev-parse HEAD) != $(git rev-parse origin/deploy-local) ]; then
   echo "🔄 Changes detected, updating..."
